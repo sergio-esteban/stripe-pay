c7b38386bdffeb913bc97f5cd92874284fe0f424
Sergio Esteban <sergio.smiling@gmail.com>
2020-01-16 19:18:47 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './App.css';
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+// import "./styles.css";
+
+toast.configure();
+
+function App() {
+  const [product] = React.useState({
+    name: "Tesla Roadster",
+    price: 12.99,
+    description: "Cool car"
+  });
+
+  async function handleToken(token) {
+    const response = await axios.post(
+      "https://qwdbs.sse.codesandbox.io/checkout",
+      { token, product }
+    );
+    const { status } = response.data;
+    console.log("Response:", response.data);
+    if (status === "success") {
+      toast("Success! Check email for details", { type: "success" });
+    } else {
+      toast("Something went wrong", { type: "error" });
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="product">
+        <h1>{product.name}</h1>
+        <h3>On Sale · ${product.price}</h3>
+      </div>
+      <StripeCheckout
+        stripeKey="pk_test_WA2zJhKHsbpIUbNZuG5G3HAo00VeumPWdH"
+        token={handleToken}
+        amount={product.price * 100}
+        name={product.name}
+        billingAddress
+        shippingAddress
+      />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
